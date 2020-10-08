fbe9acbf029a5c3aa1731d3c06c6031edaf2f643
vitsem <marie_vts@outlook.fr>
2020-10-08 19:17:30 +0200

    Add fonts

--- App.tsx
+++ App.tsx
@@ -1,19 +1,40 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { AppLoading } from 'expo';
+import { useFonts } from 'expo-font';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-    </View>
-  );
+  let [fontsLoaded] = useFonts({
+    'FiraSans_400Regular': require('./assets/fonts/FiraSans-Regular.ttf'),
+    'FiraSans_700Bold': require('./assets/fonts/FiraSans-Bold.ttf'),
+    'Montserrat_300Light': require('./assets/fonts/Montserrat-Light.ttf'),
+    'Montserrat_400Regular': require('./assets/fonts/Montserrat-Regular.ttf'),
+    'Montserrat_500Medium': require('./assets/fonts/Montserrat-Medium.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Month</Text>
+        <Text>Hello World!</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#81A7B4',
+    color: '#57606F',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    color: '#FFF',
+    fontFamily: 'FiraSans_700Bold',
+    fontSize: 30,
   },
 });
